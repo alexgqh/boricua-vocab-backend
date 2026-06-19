750d7fe53049eafee1cdc28f21583e844dd28e84
alexgqh <gymalamo@yahoo.com>
2026-06-19 14:09:37 -0400

    implement autofillData function in aiServices.js. create helper function to run prompts.

--- services/aiService.js
+++ services/aiService.js
@@ -3,38 +3,31 @@ import fs from 'node:fs'
 
 const ai = new GoogleGenAI({})
 
-async function translateContents(contents, langFrom) {
+async function runPrompt(prompt) {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.5-flash',
+    contents: prompt
+  })
+  return response.text
+}
+
+export async function translateContents(contents, langFrom) {
   const from = langFrom === 'sp' ? 'Boricua Spanish' : 'English'
   const to = langFrom === 'sp' ? 'English' : 'Boricua Spanish'
   const prompt = fs.readFileSync('prompts/translateWord.txt', 'utf8')
     .replace(/<LANG_FROM>/g, from)
     .replace(/<LANG_TO>/g, to)
     .replace(/<CONTENTS>/g, contents)
-
-  const response = await ai.models.generateContent({
-    model: 'gemini-2.5-flash',
-    contents: prompt
-  })
-
-  return response.text
+  
+  return await runPrompt(prompt)
 }
 
-function autofillData(word) {
-  return {
-    spanish: word,
-    gender: '',
-    gender_type: '',
-    english: '',
-    literal: '',
-    part_of_speech: 0,
-    theme: 0,
-    difficulty: 0,
-    note: '',
-    example: '',
-    example_translation: '',
-    vulgar: 0,
-    loan_word: 0
+export async function autofillData(wordPairs) {
+  const data = wordPairs.filter(pair => pair.en?.trim() || pair.sp?.trim())
+  if (data.length === 0) {
+    return []
   }
-}
 
-export { translateContents, autofillData }
+  const prompt = fs.readFileSync('prompts/translateWord.txt', 'utf8').replace(/<INPUT>/g, input)
+  return await runPrompt(prompt)
+}
